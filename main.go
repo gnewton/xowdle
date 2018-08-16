73edfd7db9dcb4bb7e01c52c8445ca8e87486d23
glen newton <glen.newton@gmail.com>
2018-08-16 16:16:50 -0400

    More ftp support

--- main.go
+++ main.go
@@ -16,7 +16,7 @@ var files = []string{
 	"http://www.python.org/ftp/python/2.7.3/Python-2.7.3.tgz",
 	"http://sourceforge.net/projects/pycogent/files/PyCogent/1.5.3/PyCogent-1.5.3.tgz/download",
 	"http://sourceforge.net/projects/numpy/files/NumPy/1.5.1/numpy-1.5.1.tar.gz/download",
-	"ftp://thebeast.colorado.edu/pub/biom-format-releases/biom-format-1.1.2.tar.gz",
+	//"ftp://thebeast.colorado.edu/pub/biom-format-releases/biom-format-1.1.2.tar.gz",
 	"http://www.drive5.com/uclust/uclustq1.2.22_i86linux64",
 	// WAS "https://github.com/downloads/qiime/pynast/PyNAST-1.2.tar.gz",
 	"https://github.com/biocore/pynast/archive/1.2.2.tar.gz",
@@ -31,7 +31,7 @@ var files = []string{
 	"https://sourceforge.net/projects/microbiomeutil/files/__OLD_VERSIONS/microbiomeutil_2010-04-29.tar.gz/download",
 	"http://www.mothur.org/w/images/6/6d/Mothur.1.25.0.zip",
 	"http://www.mothur.org/w/images/9/91/Clearcut.source.zip",
-	"ftp://thebeast.colorado.edu/pub/QIIME-v1.5.0-dependencies/stamatak-standard-RAxML-5_7_2012.tgz",
+	//"ftp://thebeast.colorado.edu/pub/QIIME-v1.5.0-dependencies/stamatak-standard-RAxML-5_7_2012.tgz",
 	"ftp://selab.janelia.org/pub/software/infernal/infernal.tar.gz",
 	"ftp://occams.dfci.harvard.edu/pub/bio/tgi/software/cdbfasta/cdbfasta.tar.gz",
 	"http://www.drive5.com/muscle/downloads.htm",
@@ -43,6 +43,7 @@ var files = []string{
 	"https://storage.googleapis.com/google-code-archive-downloads/v2/code.google.com/ampliconnoise/AmpliconNoiseV1.27.tar.gz",
 	"https://downloads.haskell.org/~ghc/8.4.3/ghc-8.4.3-src.tar.xz",
 	"http://cran.utstat.utoronto.ca/src/base/R-2/R-2.12.0.tar.gz",
+	"ftp://169.228.46.98/gg_12_10/gg_12_10_otus.tar.gz",
 	"ftp://greengenes.microbio.me/greengenes_release/gg_12_10/gg_12_10_otus.tar.gz",
 	//"http://gdata-python-client.googlecode.com/files/gdata-2.0.17.tar.gz",
 	"https://files.pythonhosted.org/packages/b2/e0/6e062327b211e9b1c5f30f65a9a65cf49eb1d3a7da3ce42fdc9a9e128535/gdata-2.0.17.tar.gz",
@@ -84,7 +85,10 @@ func getFtpHostFile(c chan string, wg *sync.WaitGroup) {
 	for url := range c {
 		host, dir := ftpSplit(url)
 		fmt.Println(url, host, dir)
-
+		_, _, err := ftpInfo(host, dir, dir)
+		if err != nil {
+			log.Fatal(err)
+		}
 	}
 }
 
@@ -95,7 +99,9 @@ func getHeads(c chan string, wg *sync.WaitGroup) {
 	var err error
 
 	for url := range c {
+		continue
 		fmt.Println(url)
+
 		var elapsed time.Duration
 		for j := 0; j < 5; j++ {
 			start := time.Now()
